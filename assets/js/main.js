22af54dabb1b945d4cb677e5a308b35393e0bc3f
jowned1 <jonathan.abi@hotmail.com>
2022-12-21 21:04:55 -0300

    pokedex finalizado v2 com adicao de status

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,51 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            
+            <!-- Modal -->
+            <div class="modal" id="exampleModal${pokemon.number}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-sm">
+                    <div class="modal-content pokemon ${pokemon.type}">
+                        <span class="name">${pokemon.name}</span>
+                            <div class="detail">
+                                <ol class="types">
+                                    ${pokemon.types.map((type) => `<span class="type ${type}">${type}</span>`).join('')}
+                                </ol>
+                                <span class="number">#${pokemon.number}</span>
+                            </div>
+                                <ol class="types">
+                                    <li class="pokemon ${pokemon.type}">
+                                        <img class="text-center" src="${pokemon.photo}"
+                                                    alt="${pokemon.name}">
+                                    </li>
+                                </ol>
+                                <div class="w-100 rounded basestat">
+                                   <div class="col-5 stats">
+                                        <ul>
+                                            ${pokemon.stats.map((stat) => `<li class="type">${stat}</li>`).join('')}
+                                        </ul>  
+                                   </div>
+                                   <div class="col-2 statsnum">
+                                        <ul>
+                                        ${pokemon.baseStats.map((baseStat) => `<li class="type">${baseStat}</li>`).join('')}
+                                        </ul>  
+                                   </div>
+                                   <div class="col-5">
+                                        <ul>
+                                        ${pokemon.baseStats.map((baseStat) => `<div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: ${baseStat}%" aria-valuenow="${baseStat}" aria-valuemin="0" aria-valuemax="160"></div>
+</div>`).join('')}
+                                        </ul>  
+                                   </div>
+                                </div>
+                         </div> 
+                </div>
+            </div>
+            <div class="d-grid gap-2 d-md-block">
+                <button type="button" class="btn btn-outline-dark btn-sm rounded-pill" data-bs-toggle="modal" data-bs-target="#exampleModal${pokemon.number}">
+                stats
+                </button>
+            </div>
         </li>
     `
 }
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,16 +1,25 @@
 
 const pokeApi = {}
+const urlPokemon = "https://pokeapi.co/api/v2/pokemon/1/"
+
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
+    const baseStats = pokeDetail.stats.map((statSlot) => statSlot.base_stat)
+    const stats = pokeDetail.stats.map((statSlot) => statSlot.stat.name)
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
+    const [stat] = stats
+    const baseStat = baseStats
 
     pokemon.types = types
     pokemon.type = type
+    pokemon.stats = stats
+    pokemon.stat = stat
+    pokemon.baseStats = baseStats
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
